5a5c290e6b01c945c227fbd1c665df134d1aff1a
lgcarvalho1 <lgcarvalho1@latam.stefanini.com>
2020-08-05 11:58:51 -0300

    fixes

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,15 @@ const getBookSummary = (summary) => {
   return summary;
 };
 const BookCard = ({ book }) => (
-  <Card style={{ width: 400, alignItems: 'center' }}>
+  <Card style={{ width: 300, alignItems: 'center' }}>
     <CardHeader style={{ fontSize: 19, width: '100%' }}>{book.book}</CardHeader>
     <CardBody>
       <img src={book.img} width={120} height={170} alt="img" />
     </CardBody>
-    <CardText style={{ fontSize: 18 }}>
+    <CardText style={{
+      fontSize: 18, paddingTop: '1%', paddingLeft: '4%', paddingRight: '4%', paddingBottom: '2%',
+    }}
+    >
       {getBookSummary(book.summary)}
     </CardText>
   </Card>
@@ -35,7 +38,7 @@ const Home = ({ books }) => (
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet" />
     <Container className="align-content-center align-self-center">
       <Col className="mt-4 mb-3" style={{ textAlign: 'center' }}><h1>ReadIt</h1></Col>
-      <Container className="row justify-content-center">
+      <Container className="row justify-content-center" style={{ marginRight: 0, marginLeft: 0 }}>
         {books.map((book) => <BookCard book={book} />)}
       </Container>
     </Container>
